beeb8850eb4ff4e809c39b6983140235362715d0
jhjh81704 <jhjh81704@gmail.com>
2019-05-01 23:39:56 +0800

    add header

--- products.py
+++ products.py
@@ -18,7 +18,13 @@ while True:
     products.append([name, price])  # 把上面三行程式碼成這一行
 print(products)
 
-products[0][0]  # procdutcs [] 裡的第 0 個 跟[] 裡面 [] 的第 0 個
+# products[0][0]  # procdutcs [] 裡的第 0 個 跟[] 裡面 [] 的第 0 個
 
 for p in products:
     print(p[0], '的價格是', p[1])
+    
+
+with open('products.csv', 'w', encoding = 'utf-8') as f:  # with 有 close 的意思，使用完檔案之後就會 close
+    f.write('商品,價格\n') 
+    for p in products:
+        f.write(p[0] + ',' + p[1] + '\n')
